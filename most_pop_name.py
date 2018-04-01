a21008fdd694185a7d5c9bc54ea4e4a1b00fca1d
Lynda Anne <lsefton62@gmail.com>
2018-04-01 14:42:10 -0400

    top female names graphs done 1880-2016

--- most_pop_name.py
+++ most_pop_name.py
@@ -75,11 +75,11 @@ fnames_2000_2016_tidy = top_fnames_21st_cent.pivot_table(values='pct_name', inde
 fnames_2000_2016_tidy = fnames_2000_2016_tidy.fillna(0)
 fnames_2000_2016_tidy= fnames_2000_2016_tidy.reset_index()
 ## fnames_2000_2016_tidy= fnames_2000_2016_tidy.data_sort()
-# print(fnames_2000_2016_tidy.tail())
-# top_fnames_21st_cent= fnames_2000_2016_tidy.set_index('Year')
-# print(top_fnames_21st_cent.tail())
+print(fnames_2000_2016_tidy.tail())
 print(fnames_2000_2016_tidy.info())
-
+# print(top_fnames_21st_cent.tail())
+# print(top_fnames_21st_cent.info())
+# print(top_fnames_21st_cent['Name'].drop_duplicates())
 
 # print(fnames_2000_2016_tidy['Year'])
 # print(fnames_2000_2016_tidy.iloc[:, 8 ])
@@ -101,7 +101,7 @@ def top_girls_names_1():
     plt.ylim(-0.05, 0.7)
     plt.subplots_adjust(left=0.1)
     plt.ylabel('Pecent of Names')
-    plt.title('Top Girls Names Chart 1 of 8')
+    plt.title('Top Girls Names Chart 1 of 10')
     plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
     plt.margins(0.2)
     plt.savefig('scatter_top_girls_names1.pdf')
@@ -111,7 +111,7 @@ def top_girls_names_2():
     colors=['red', 'blue', 'lime', 'deeppink']
     n= 5
     for item in fnames_2000_2016_tidy :
-        if n <= 9 :
+        if n <= 8 :
             x= fnames_2000_2016_tidy['Year']
             y = fnames_2000_2016_tidy.iloc[0:, n]
             plt.style.use('ggplot')
@@ -123,7 +123,7 @@ def top_girls_names_2():
     plt.ylim(-0.05, 0.7)
     plt.subplots_adjust(left=0.1)
     plt.ylabel('Pecent of Names')
-    plt.title('Top Girls Names Chart 2 of 8')
+    plt.title('Top Girls Names Chart 2 of 10')
     plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
     plt.margins(0.2)
     plt.savefig('scatter_top_girls_names2.pdf')
@@ -131,9 +131,9 @@ def top_girls_names_2():
 
 def top_girls_names_3():
     colors=['red', 'blue', 'lime', 'deeppink']
-    n= 10
+    n= 9
     for item in fnames_2000_2016_tidy :
-        if n <= 14 :
+        if n <= 12 :
             x= fnames_2000_2016_tidy['Year']
             y = fnames_2000_2016_tidy.iloc[0:, n]
             plt.style.use('ggplot')
@@ -144,7 +144,7 @@ def top_girls_names_3():
     plt.ylim(-0.05, 0.7)
     plt.subplots_adjust(left=0.1)
     plt.ylabel('Pecent of Names')
-    plt.title('Top Girls Names Chart 3 of 8')
+    plt.title('Top Girls Names Chart 3 of 10')
     plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
     plt.margins(0.2)
     plt.savefig('scatter_top_girls_names3.pdf')
@@ -152,9 +152,9 @@ def top_girls_names_3():
 
 def top_girls_names_4():
     colors=['red', 'blue', 'lime', 'deeppink']
-    n= 15
+    n= 13
     for item in fnames_2000_2016_tidy :
-        if n <= 19 :
+        if n <= 16 :
             x= fnames_2000_2016_tidy['Year']
             y = fnames_2000_2016_tidy.iloc[0:, n]
             plt.style.use('ggplot')
@@ -166,7 +166,7 @@ def top_girls_names_4():
     plt.ylim(-0.05, 0.7)
     plt.subplots_adjust(left=0.1)
     plt.ylabel('Pecent of Names')
-    plt.title('Top Girls Names Chart 4 of 8')
+    plt.title('Top Girls Names Chart 4 of 10')
     plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
     plt.margins(0.2)
     plt.savefig('scatter_top_girls_names4.pdf')
@@ -174,9 +174,9 @@ def top_girls_names_4():
 
 def top_girls_names_5():
     colors=['red', 'blue', 'lime', 'deeppink']
-    n= 20
+    n= 17
     for item in fnames_2000_2016_tidy :
-        if n <= 25 :
+        if n <= 20 :
             x= fnames_2000_2016_tidy['Year']
             y = fnames_2000_2016_tidy.iloc[0:, n]
             plt.style.use('ggplot')
@@ -187,7 +187,7 @@ def top_girls_names_5():
     plt.ylim(-0.05, 0.7)
     plt.subplots_adjust(left=0.1)
     plt.ylabel('Pecent of Names')
-    plt.title('Top Girls Names Chart 5 of 8')
+    plt.title('Top Girls Names Chart 5 of 10')
     plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
     plt.margins(0.2)
     plt.savefig('scatter_top_girls_names5.pdf')
@@ -195,9 +195,9 @@ def top_girls_names_5():
 
 def top_girls_names_6():
     colors=['red', 'blue', 'lime', 'deeppink']
-    n= 26
+    n= 21
     for item in fnames_2000_2016_tidy :
-        if n <= 30 :
+        if n <= 24 :
             x= fnames_2000_2016_tidy['Year']
             y = fnames_2000_2016_tidy.iloc[0:, n]
             plt.style.use('ggplot')
@@ -208,7 +208,7 @@ def top_girls_names_6():
     plt.ylim(-0.05, 0.7)
     plt.subplots_adjust(left=0.1)
     plt.ylabel('Pecent of Names')
-    plt.title('Top Girls Names Chart 6 of 8')
+    plt.title('Top Girls Names Chart 6 of 10')
     plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
     plt.margins(0.2)
     plt.savefig('scatter_top_girls_names6.pdf')
@@ -216,9 +216,9 @@ def top_girls_names_6():
 
 def top_girls_names_7():
     colors=['red', 'blue', 'lime', 'deeppink']
-    n= 31
+    n= 25
     for item in fnames_2000_2016_tidy :
-        if n <= 35 :
+        if n <= 28 :
             x= fnames_2000_2016_tidy['Year']
             y = fnames_2000_2016_tidy.iloc[0:, n]
             plt.style.use('ggplot')
@@ -229,7 +229,7 @@ def top_girls_names_7():
     plt.ylim(-0.05, 0.7)
     plt.subplots_adjust(left=0.1)
     plt.ylabel('Pecent of Names')
-    plt.title('Top Girls Names Chart 7 of 8')
+    plt.title('Top Girls Names Chart 7 of 10')
     plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
     plt.margins(0.2)
     plt.savefig('scatter_top_girls_names7.pdf')
@@ -237,7 +237,49 @@ def top_girls_names_7():
 
 def top_girls_names_8():
     colors=['red', 'blue', 'lime', 'deeppink']
-    n= 36
+    n= 29
+    for item in fnames_2000_2016_tidy :
+        if n <= 32 :
+            x= fnames_2000_2016_tidy['Year']
+            y = fnames_2000_2016_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+    # plt.xticks(rotation='vertical')
+    # plt.xlim(2009, 2018)
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 8 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_girls_names8.pdf')
+    plt.show()
+
+def top_girls_names_9():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n= 33
+    for item in fnames_2000_2016_tidy :
+        if n <= 36 :
+            x= fnames_2000_2016_tidy['Year']
+            y = fnames_2000_2016_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+    # plt.xticks(rotation='vertical')
+    # plt.xlim(2009, 2018)
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 9 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_girls_names9.pdf')
+    plt.show()
+
+def top_girls_names_10():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n= 37
     for item in fnames_2000_2016_tidy :
         if n <= 39 :
             x= fnames_2000_2016_tidy['Year']
@@ -250,20 +292,278 @@ def top_girls_names_8():
     plt.ylim(-0.05, 0.7)
     plt.subplots_adjust(left=0.1)
     plt.ylabel('Pecent of Names')
-    plt.title('Top Girls Names Chart 8 of 8')
+    plt.title('Top Girls Names Chart 10 of 10')
     plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
     plt.margins(0.2)
-    plt.savefig('scatter_top_girls_names8.pdf')
+    plt.savefig('scatter_top_girls_names10.pdf')
     plt.show()
 
-top_girls_names_1()
-top_girls_names_2()
-top_girls_names_3()
-top_girls_names_4()
-top_girls_names_5()
-top_girls_names_6()
-top_girls_names_7()
-top_girls_names_8()
+# top_girls_names_1()
+# top_girls_names_2()
+# top_girls_names_3()
+# top_girls_names_4()
+# top_girls_names_5()
+# top_girls_names_6()
+# top_girls_names_7()
+# top_girls_names_8()
+# top_girls_names_9()
+# top_girls_names_10()
 
-#_______________________________________________________________________________
-#
+
+names= top_fnames_21st_cent['Name'].drop_duplicates()
+# print(names)
+
+fnames_year= fnames_year.reset_index()
+fnames_year= fnames_year.set_index('Name')
+tops= fnames_year.loc[names]
+
+del tops['Sex']
+del tops['Count']
+# tops_group= tops.groupby('Name')
+
+tops=tops.reset_index()
+
+
+tops_group_tidy = tops.pivot_table(values='pct_name', index=['Year'], columns=['Name'])
+tops_group_tidy = tops_group_tidy.fillna(0)
+tops_group_tidy= tops_group_tidy.reset_index()
+
+print(tops_group_tidy.info())
+print(tops_group_tidy.head())
+
+def tops_years1():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n=1
+    for item in tops_group_tidy :
+        if n <= 4 :
+            x= tops_group_tidy['Year']
+            y = tops_group_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+    # plt.xticks(rotation='vertical')
+    # plt.xlim(2009, 2018)
+
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 1 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_g_names1.pdf')
+    plt.show()
+
+def tops_years2():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n=5
+    for item in tops_group_tidy :
+        if n <= 8 :
+            x= tops_group_tidy['Year']
+            y = tops_group_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+    # plt.xticks(rotation='vertical')
+    # plt.xlim(2009, 2018)
+
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 2 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_g_names2.pdf')
+    plt.show()
+
+def tops_years3():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n= 9
+    for item in tops_group_tidy :
+        if n <= 12 :
+            x= tops_group_tidy['Year']
+            y = tops_group_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+    # plt.xticks(rotation='vertical')
+    # plt.xlim(2009, 2018)
+
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 3 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_g_names3.pdf')
+    plt.show()
+
+def tops_years4():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n= 13
+    for item in tops_group_tidy :
+        if n <= 16 :
+            x= tops_group_tidy['Year']
+            y = tops_group_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+    # plt.xticks(rotation='vertical')
+    # plt.xlim(2009, 2018)
+
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 4 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_g_names4.pdf')
+    plt.show()
+
+def tops_years5():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n= 17
+    for item in tops_group_tidy :
+        if n <= 20 :
+            x= tops_group_tidy['Year']
+            y = tops_group_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+    # plt.xticks(rotation='vertical')
+    # plt.xlim(2009, 2018)
+
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 5 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_g_names5.pdf')
+    plt.show()
+
+def tops_years6():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n= 21
+    for item in tops_group_tidy :
+        if n <= 24 :
+            x= tops_group_tidy['Year']
+            y = tops_group_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+    # plt.xticks(rotation='vertical')
+    # plt.xlim(2009, 2018)
+
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 6 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_g_names6.pdf')
+    plt.show()
+
+def tops_years7():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n= 25
+    for item in tops_group_tidy :
+        if n <= 28 :
+            x= tops_group_tidy['Year']
+            y = tops_group_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+    # plt.xticks(rotation='vertical')
+    # plt.xlim(2009, 2018)
+
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 7 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_g_names7.pdf')
+    plt.show()
+
+def tops_years8():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n= 29
+    for item in tops_group_tidy :
+        if n <= 32 :
+            x= tops_group_tidy['Year']
+            y = tops_group_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+        # plt.xticks(rotation='vertical')
+        # plt.xlim(2009, 2018)
+
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 8 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_g_names8.pdf')
+    plt.show()
+
+
+def tops_years9():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n= 32
+    for item in tops_group_tidy :
+        if n <= 35 :
+            x= tops_group_tidy['Year']
+            y = tops_group_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+        # plt.xticks(rotation='vertical')
+        # plt.xlim(2009, 2018)
+
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 9 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_g_names9.pdf')
+    plt.show()
+
+def tops_years10():
+    colors=['red', 'blue', 'lime', 'deeppink']
+    n= 36
+    for item in tops_group_tidy :
+        if n <= 39 :
+            x= tops_group_tidy['Year']
+            y = tops_group_tidy.iloc[0:, n]
+            plt.style.use('ggplot')
+            plt.scatter(x, y)
+            n= n + 1
+        # plt.xticks(rotation='vertical')
+        # plt.xlim(2009, 2018)
+
+    plt.ylim(-0.05, 0.7)
+    plt.subplots_adjust(left=0.1)
+    plt.ylabel('Pecent of Names')
+    plt.title('Top Girls Names Chart 10 of 10')
+    plt.legend(loc='best', fontsize='xx-small', markerscale=0.7)
+    plt.margins(0.2)
+    plt.savefig('scatter_top_g_names10.pdf')
+    plt.show()
+
+
+tops_years1()
+tops_years2()
+tops_years3()
+tops_years4()
+tops_years5()
+tops_years6()
+tops_years7()
+tops_years8()
+tops_years9()
+tops_years10()
+
+# #______tops_years4()
+# _________________________________________________________________________
+# #
